6aa75c3d3b18f00b68ea078ee6dbc45d78640601
osvaldo345 <jrincon15@ucol.mx>
2019-02-07 19:21:22 -0600

    avamce

--- conceptos basicos/auto.js
+++ conceptos basicos/auto.js
@@ -1,9 +1,33 @@
-class auto {
-condtructor (marca, modelo, color, kilometraje){
+export default class auto {
+constructor (marca, modelo, color="blanco", kilometraje = 0){
     this.marca = marca;
     this.modelo = modelo;
     this.color = color;
     this.kilometraje = kilometraje;
-    this.estado  "apagado";
+    this.estado = "apagado";
+    }
+
+    mostrarEstado(){
+        console.log(`El auto ${this.marca} ${this.color} del año ${this.modelo} tiene un kilometraje de ${this.kilometraje} kms y esta ${this.estado}`);
+    }
+
+    encender(){
+        this.estado = "Encendido";
+        this.mostrarEstado();
+
+    }
+    apagar() {
+        this.estado = "Apagado";
+        this.mostrarEstado();
+    }
+
+    avanzar(velocidad, tiempo) {
+        if(this.estado === "apagado") {
+            console.log("El auto no puede avanzar porque esta apagado");
+        }else {
+            let distancia = velocidad * tiempo;
+            this.kilometraje = this.kilometraje + distancia;
+            this.mostrarEstado();
+        }
     }
 }
--- conceptos basicos/main.js
+++ conceptos basicos/main.js
@@ -1,1 +1,14 @@
-console.log("hola, Mundo!");
+import auto from "./auto.js";
+
+let auto1 = new auto("Ford", 2000, "rojo", 100);
+let auto2 = new auto("Nissan", 2010, "azul");
+let auto3 = new auto("Toyota", "2018");
+
+auto1.encender();
+auto1.avanzar(50, 1.5);
+auto1.apagar();
+auto1.avanzar(50, 1.5);
+
+console.log(`kilometraje ${auto1.kilometraje}`);
+auto1.kilometraje = 5;
+console.log(`kilometraje ${auto1.kilometraje}`);
